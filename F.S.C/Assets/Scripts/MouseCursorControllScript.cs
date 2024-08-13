8918c5c616fb2e2ebf56bc7c349644d22458066a
Morita-340 <60276263.molita.cgp@gmail.com>
2024-08-13 16:48:56 +0900

    クリックからのドラッグ時のみプレビューが表示される機能を実装した。
    MouseCursolControllerでは入力タイミングの制御、SnapToGridでグリッド補正をするようにした。
    MouseCursolControllerは今の状態にこぎつけるのにかなり苦労したので、リファクタリング以外で無暗に変更しない方がいい

--- F.S.C/Assets/Scripts/MouseCursorControllScript.cs
+++ F.S.C/Assets/Scripts/MouseCursorControllScript.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// マウスカーソルによる入力はここで管理する
+/// </summary>
 public class MouseCursorControllScript : MonoBehaviour
 {
+    [SerializeField]
+    GameObject UnitSimulater;
+    SnapToGrid snapToGrid;
+    PlayerUnitSimulateScript playerUnitSimulateScript;
+    [SerializeField]
+    GameObject PreviewUnit;
     GameObject ParentObject = null;
     private Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        snapToGrid = UnitSimulater.GetComponent<SnapToGrid>();
+        playerUnitSimulateScript = UnitSimulater.GetComponent<PlayerUnitSimulateScript>();
     }
 
     // Update is called once per frame
@@ -18,6 +29,7 @@ public class MouseCursorControllScript : MonoBehaviour
         float wheelInput = Input.GetAxis("Mouse ScrollWheel");
         //Debug.Log(Input.mousePosition);
         target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,10));
+        Debug.Log("bbbZ" + target);
         CursorDragClick(wheelInput);
     }
     /// <summary>
@@ -26,28 +38,74 @@ public class MouseCursorControllScript : MonoBehaviour
     void CursorDragClick(float wheelInput){
         Ray ray= Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit2D = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
-        if(Input.GetMouseButton(0)){
-            Debug.Log("aaa");
-            if(hit2D.collider/*撃破後のUnitであることを識別できる何かをフラグに持ってくる*/){
-                ParentObject = hit2D.collider.gameObject.transform.root.gameObject;
-                Debug.Log("bbb");
+        //Debug.Log(hit2D.collider.gameObject.name);
+        List<Vector3> positionList = new List<Vector3>();
+        List<Quaternion> rotationList = new List<Quaternion>();
+        if(Input.GetMouseButtonDown(0)){
+            ParentObject = hit2D.collider.gameObject.transform.root.gameObject;
+            if(hit2D.collider.tag == "DestroyedUnit"/*撃破後のUnitであることを識別できる何かをフラグに持ってくる*/){
+                //子オブジェクトのtransformを取得する
+                for(int i = 0;i < ParentObject.transform.childCount;i++){
+                        //Transform child = ParentObject.transform.GetChild(i);
+                    Vector3 position = ParentObject.transform.GetChild(i).localPosition;
+                    Quaternion rotation = ParentObject.transform.GetChild(i).localRotation;
+                    //position = snapToGrid.SnapPositon(position);
+                    //rotation = snapToGrid.SnapRotation(rotation);
+                    positionList.Add(position);
+                    rotationList.Add(rotation);
+                        //child.position = snapToGrid.SnapPositon(child.position);
+                        //child.rotation = snapToGrid.SnapRotation(child.rotation);
+                        //ChildrenList.Add(child);
+                }
+                Debug.Log("bbbA" + UnitSimulater.transform.position);
+                for(int i = 0;i < positionList.Count;i++){
+                    GameObject Preview = Instantiate(PreviewUnit,positionList[i],rotationList[i]);
+                    Preview.transform.SetParent(UnitSimulater.transform,false);
+                }
+                //InstantiateでSimulaterの子オブジェクトとしてprafabのプレビュー用オブジェクトを配置する
+                /*
+                foreach(Transform child in ChildrenList){
+                Instantiate(PrviewUnit,child.position,child.rotation,UnitSimulater.transform);
+                }
+                */
+                //PlayerUnitSimulaterがプレビュー表示するためにオブジェクト情報をコピーしてSimulater側で複製する
+
+                //Rotationは0、座標は原点中心とする
+                Debug.Log("bbbA"+ ParentObject.transform.position);
             }
-            if(ParentObject != null){
+        }
+        if(Input.GetMouseButton(0)){
+            Debug.Log("bbbB"+ ParentObject.name + ParentObject.transform.position);
+            Debug.Log("aaa");
+            if(ParentObject == null){
+                Debug.LogWarning("Clicked Parent Object is null!");
+            }
+            else{
                 EnemyUnitManagementScript enemyUnitManagementScript = ParentObject.GetComponent<EnemyUnitManagementScript>();
+                if(enemyUnitManagementScript == null){
+                    Debug.LogWarning("enemyUnitManagementScript is null" + ParentObject.name);
+                }else{
                 //親オブジェクトのスクリプトEnemyUnitManagementScriptにアクセス
                 enemyUnitManagementScript.MovePosition(target);
                 //マウスに追従するようにするMovePosition関数を呼び出す
                 enemyUnitManagementScript.Spin(wheelInput);
                 Debug.Log("ccc");
+                }
             }
         }
         if(Input.GetMouseButtonUp(0)){
+            //Simulaterの複製を削除する
+            for(int i = 0;i < UnitSimulater.transform.childCount;i++){
+                Destroy(UnitSimulater.transform.GetChild(i).gameObject);
+            }
+            //PlayerUnitに複製する
             ParentObject = null;
             Debug.Log("ddd");
         }
     }
     /// <summary>
     /// マウスホイールでドラッグ中のオブジェクトを回転させる
+    /// EnemyUnitManagementScript.Spinで対応させたので現在不要
     /// </summary>
     void CursorRotate(){
 
--- F.S.C/Assets/Scripts/SnapToGrid.cs
+++ F.S.C/Assets/Scripts/SnapToGrid.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// UnitSimulaterの回転と移動を制御しているだけ
+/// このクラスもといコンポーネントはオブジェクトにアタッチして2つのTransformを設定することで、グリッドの起点（中心）オブジェクト（プレイヤー）からマス目の等間隔に補正対象のオブジェクトのプレビュー用オブジェクトを動的に設置することが可能
+/// </summary>
 public class SnapToGrid : MonoBehaviour
 {
-    public Transform gridOrigin; // グリッドの起点（中心）オブジェクト
+    public Transform gridOrigin; // グリッドの起点（中心）オブジェクト[SerializeField]に書き換えても問題ない
     [SerializeField]
-    Transform Origin;
+    Transform Origin;//プレビューで補正する対象のオブジェクト
     public float cellSize; // グリッドのセルのサイズ（正方形）
     public bool snapRotation = true; // 回転もスナップするかどうか
 
     void Update()
     {
+        //プレビュー表示範囲に入ったら、入ったタイミングに一度だけ対応する子オブジェクトを作成して設定を済ませる。範囲から出るか設置するかキャンセルをすることで子オブジェクトを消去する
         SnapObjectToGrid();
     }
-
+    /// <summary>
+    /// このオブジェクトを動的にグリッド補正、角度補正する
+    /// </summary>
     void SnapObjectToGrid()
     {
         if (gridOrigin == null)
@@ -29,9 +36,9 @@ public class SnapToGrid : MonoBehaviour
         // 位置のスナップ
         float snappedX = Mathf.Round(localPosition.x / cellSize) * cellSize;
         float snappedY = Mathf.Round(localPosition.y / cellSize) * cellSize;
-        float snappedZ = Mathf.Round(localPosition.z / cellSize) * cellSize;
+        //float snappedZ = Mathf.Round(localPosition.z / cellSize) * cellSize;
 
-        Vector3 snappedLocalPosition = new Vector3(snappedX, snappedY, snappedZ);
+        Vector3 snappedLocalPosition = new Vector3(snappedX, snappedY, 7);
         
         // グリッドのワールド座標に変換
         transform.position = gridOrigin.TransformPoint(snappedLocalPosition);
@@ -43,11 +50,59 @@ public class SnapToGrid : MonoBehaviour
             //float snappedRotationX = Mathf.Round(eulerRotation.x / 90) * 90;
             //float snappedRotationY = Mathf.Round(eulerRotation.y / 90) * 90;
             //float snappedRotationZ = Mathf.Round(eulerRotation.z / 90) * 90;
-            float snappedRotationZ = gridOrigin.transform.eulerAngles.z % 90 + Mathf.Floor(eulerRotation.z / 90) * 90;
+            float snappedRotationZ = gridOrigin.transform.eulerAngles.z % 90 + Mathf.Round(eulerRotation.z/90) * 90;
             //(eulerRotation.z+90) / 90
-            Debug.Log("a"+snappedRotationZ+"b"+gridOrigin.transform.eulerAngles.z+"c"+(Mathf.Floor(eulerRotation.z / 90) * 90));
+            //Debug.Log("a"+snappedRotationZ+"b"+gridOrigin.transform.eulerAngles.z+"c"+(Mathf.Floor(eulerRotation.z / 90) * 90));
             //this.gameObject.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, snappedRotationZ);
             this.gameObject.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, snappedRotationZ);
         }
+    }
+    /// <summary>
+    /// クリックしてプレビューを初めて表示する際にもグリッド補正が必要なのでこのメソッドを呼び出す
+    /// </summary>
+    /// <param name="position">グリッド補正後の座標</param>
+    /// <returns></returns>
+    public Vector3 SnapPositon(Vector3 position){
+        Vector3 localPosition = position;
+
+        // 位置のスナップ
+        float snappedX = Mathf.Round(localPosition.x / cellSize) * cellSize;
+        float snappedY = Mathf.Round(localPosition.y / cellSize) * cellSize;
+        //float snappedZ = Mathf.Round(localPosition.z / cellSize) * cellSize;
+
+        Vector3 snappedLocalPosition = new Vector3(snappedX, snappedY, 7);
+        
+        // グリッドのワールド座標に変換
+        return gridOrigin.TransformPoint(snappedLocalPosition);
+    }
+    /// <summary>
+    /// クリックしてプレビューを初めて表示する際にもグリッド補正が必要なのでこのメソッドを呼び出す
+    /// The first time you click to display the preview, you also need to correct the grid, so call this method
+    /// </summary>
+    /// <param name="rotation">グリッド補正後の回転角</param>
+    /// <returns></returns>
+    public Quaternion SnapRotation(Quaternion rotation){
+        Vector3 eulerRotation = rotation.eulerAngles;
+        //float snappedRotationX = Mathf.Round(eulerRotation.x / 90) * 90;
+        //float snappedRotationY = Mathf.Round(eulerRotation.y / 90) * 90;
+        //float snappedRotationZ = Mathf.Round(eulerRotation.z / 90) * 90;
+        float snappedRotationZ = gridOrigin.transform.eulerAngles.z % 90 + Mathf.Floor(eulerRotation.z/90) * 90;
+        //(eulerRotation.z+90) / 90
+        //Debug.Log("a"+snappedRotationZ+"b"+gridOrigin.transform.eulerAngles.z+"c"+(Mathf.Floor(eulerRotation.z / 90) * 90));
+        //this.gameObject.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, snappedRotationZ);
+            
+        return Quaternion.Euler(eulerRotation.x, eulerRotation.y, snappedRotationZ);
+    }
+    public Vector3 SnapRotation(Vector3 rotation){
+        Vector3 eulerRotation = rotation;
+        //float snappedRotationX = Mathf.Round(eulerRotation.x / 90) * 90;
+        //float snappedRotationY = Mathf.Round(eulerRotation.y / 90) * 90;
+        //float snappedRotationZ = Mathf.Round(eulerRotation.z / 90) * 90;
+        float snappedRotationZ = gridOrigin.transform.eulerAngles.z % 90 + Mathf.Floor(eulerRotation.z/90) * 90;
+        //(eulerRotation.z+90) / 90
+        //Debug.Log("a"+snappedRotationZ+"b"+gridOrigin.transform.eulerAngles.z+"c"+(Mathf.Floor(eulerRotation.z / 90) * 90));
+        //this.gameObject.transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, snappedRotationZ);
+            
+        return new Vector3(eulerRotation.x, eulerRotation.y, snappedRotationZ);
     }
 }
